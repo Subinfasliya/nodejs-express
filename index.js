5668d6cd7347138b1dfc4811a99fd70a165fe95b
Subin <subinfasliya@gmail.com>
2026-07-01 21:28:15 +0530

    chore: add package.json with express dependency

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!");
+const express = require("express");
+
+const PORT = 5000;
+
+const app = express();
+
+app.get("/",(req,res) => {
+    res.send("Home Page")
+})
+
+app.get("/about-us",(req,res) => {
+    res.send("About Page")
+})
+
+app.get("/contact-us",(req,res) => {
+    res.send("Contact Page")
+})
+
+app.listen(PORT,()=> {
+    console.log("Server Running...");
+})
+
