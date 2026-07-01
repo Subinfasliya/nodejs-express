f63def712b782475c01c4c58515b05633cc72a41
Subin <subinfasliya@gmail.com>
2026-07-01 21:08:39 +0530

    Set up basic HTTP server and add package.json

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+const http = require("node:http")
+
+const port = 5000;
+
+const server = http.createServer((req,res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Hello World!")
+})
+
+server.listen(port,()=> {
+    console.log("Server Running Successfully");
+    
+})
