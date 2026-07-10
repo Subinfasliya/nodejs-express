208bc1245a849339fe304be3dde7b85f8fe547dc
Subin <subinfasliya@gmail.com>
2026-07-10 22:01:29 +0530

    feat: set up Express server and MongoDB connection

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express")
+const connectDB = require("./config/db")
+require("dotenv").config()
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+connectDB()
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running successfully`);
+    
+})
