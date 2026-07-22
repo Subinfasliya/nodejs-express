3e7626e2d484656dad0a9524c3cc1367a80a7b24
Subin <subinfasliya@gmail.com>
2026-07-22 21:04:38 +0530

    feat: set up Express server and define root API endpoint

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 5000
+
+app.get("/", (req,res) => {
+    res.status(200).json({
+        message:"API Calling..."
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running successfully on port ${PORT}`);
+    
+})
