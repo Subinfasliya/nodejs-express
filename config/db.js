12471e14d9a873dbe60f7f1f9b12a99767362b21
Subin <subinfasliya@gmail.com>
2026-07-24 20:45:23 +0530

    feat: implement MongoDB connection and update server setup

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+const connectDB = async()=> {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+        console.log("Database connected...");
+        
+    } catch (error) {
+        console.log("Database not connected", error);
+        
+    }
+}
+
+module.exports = connectDB
